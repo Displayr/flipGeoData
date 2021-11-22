9367aa4ccc0886cb39c4a36b98895dc821f7f566
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-11-22 19:52:16 +1100

    DS-3540: Add australia post code data

---
+++ R/australia.post.codes.R
@@ -0,0 +1,25 @@
+#' Postcodes for Australia.
+#'
+#' Postcodes, approximate latitude/longitude, state, suburb, region, and LGA info for cities and towns in Australia.
+#'
+#' \itemize{
+#' \item place - character string place name
+#' \item post.code - character post code for \code{place}
+#' \item state - Factor with 8 levels; state/1st order administrative subdivision
+#' \item province - Character; suburb/2nd order administrative subdivision
+#' \item suburb - Character; suburb/locality/community geographic subdivision; some entries are missing.
+#' \item latitude - Numeric, approximate latitude for region/place
+#' \item longitude - Numeric, approximate longitude for region/place
+#' \item LGA - Local government area; third level of government in Australia; e.g. shires,
+#' cities, town, municipal council, etc. All entries in the ACT are given the value
+#' \code{"Australian Capital Territory"}.
+#' \item region - Factor with 79 levels containing the geographic region within each state for
+#' \code{place}. N/A for external territories of Australia .
+#' }
+#' @name australia.post.codes
+#' @format A data.frame with 16,873 rows and 8 columns.
+#' @usage data(euro.post.codes)
+#' @docType data
+#' @keywords datasets
+#' @source \url{https://download.geonames.org/export/zip/} and \url{https://wikipedia.org}
+"australia.post.codes"
--- data-raw/australia.post.codes.R
+++ data-raw/australia.post.codes.R
@@ -1,3 +1,8 @@
+library(xml2)
+library(httr)
+library(FNN)
+library(tidygeocoder)
+
 fname <- "AU.zip"
 if (!file.exists(fname))
 {
@@ -26,61 +31,165 @@ simpleCap <- function(x) {
 }
 australia.post.codes[["suburb"]] <- vapply(australia.post.codes[["suburb"]],
                                            simpleCap, "", USE.NAMES = FALSE)
+missing.idx <- australia.post.codes[["suburb"]] == "NANA"
+australia.post.codes[["suburb"]][missing.idx] <- NA
 australia.post.codes[["state"]] <- as.factor(australia.post.codes[["state"]])
 
+
+################################################################################
+## Use Wikipedia to get LGA and region info
+################################################################################
 states <- levels(australia.post.codes[["state"]])
-
-state_url_part <- gsub(" ", "_", states)[!states %in% "Australian Capital Territory"]
-
-## Victoria's Wiki page has multiple tables, one for Melbourne and one for each region
+## Note: There are no LGAs for ACT
+state_url_part <- states[!states %in% "Australian Capital Territory"]
+state_url_part <- gsub(" ", "_", state_url_part)
+nt.idx <- state_url_part %in% "Northern_Territory"
+state_url_part[nt.idx] <- paste0("the_", state_url_part[nt.idx])
+## Victoria's, Western Australia and South Australia's Wiki pages have multiple tables, one for
+## Melbourne/Perth/Adelaide and one for each region
 ## other states have a single table
-## No LGAs for ACT
-url <- paste0("https://en.wikipedia.org/wiki/",
-              "Local_government_areas_of_Victoria")
-pl <- content(GET(url))  # , encoding = "UTF-8")
-lga.tables <- xml_find_all(pl, "//table[@class='wikitable sortable']")
-melb.regions <- xml_text(xml_find_all(lga.tables[1], ".//tr/td[3]"))
-other.regions <- xml_text(xml_find_all(pl, "//h3/span[@class='mw-headline']/a"))
-n.lgas <- sapply(lga.tables[-1], function(tbl) length(xml_find_all(tbl, ".//tr/td[1]")))
-lga.names <- xml_text(xml_find_all(lga.tables, ".//tr/td[1]"))
-regions <- c(melb.regions, unlist(lapply(1:length(n.lgas), function(i) rep(other.regions[i],
-                                                                           n.lgas[i]))))
-lgas <- data.frame(state = "Victoria", region = regions, lga = lga.names)
-## Add other states with a single table
-state_url_part <- gsub(" ", "_", states)[!states %in% c("Australian Capital Territory", "Victoria")]
-for (state in rev(state_url_part))
+
+
+## Some tables have a column for Region, otherwise it must be read from
+## preceding least-important heading
+findRegions <- function(xml.table)
 {
+    ## some tables have region in 2nd col, some in 3rd
+    headers <- xml_text(xml_find_all(xml.table, ".//th"))
+    region.idx <- grep("^Region", headers)[1]
+    if (!is.na(region.idx))
+        return(trimws(xml_text(xml_find_all(xml.table,
+                                            paste0(".//tr/td[", region.idx, "]")))))
+
+    n.lgas <- length(xml_find_all(xml.table, ".//tr/td[1]"))
+    ## Determine region from least important heading on page
+    region <- xml_text(xml_find_first(xml.table, "preceding-sibling::h4[1]"))
+    if (is.na(region))  # Only happens for Perth/Western Australia table
+        region <- xml_text(xml_find_first(xml.table, "preceding-sibling::h3[1]"))
+    if (is.na(region))
+    {
+        region <- xml_text(xml_find_first(xml.table, "preceding-sibling::h2[1]"))
+        if (grepl("^Metropolitan LGAs", region)) {
+            region <- "Perth"
+        }else
+            stop("Couldn't determine region")
+    }
+
+    region <- sub("\\[edit\\]$", "", region)
+    return(rep(region, n.lgas))
+}
+
+lgas <- NULL
+for (i in seq_along(state_url_part))
+{
+    state <- state_url_part[i]
     url <- paste0("https://en.wikipedia.org/wiki/",
                   ifelse(state == "New_South_Wales", "List_of_local_government_areas_in_",
                          "Local_government_areas_of_"), state)
     pl <- content(GET(url), encoding = "UTF-8")
-    lga.table <- xml_find_all(pl, "//table[@class='wikitable sortable']")
-    if (state == "Western_Australia")
-        lga.table <- lga.table[2]
-    ## some tables have region in 2nd col, some in 3rd
-    headers <- xml_text(xml_find_all(lga.table, ".//th"))
-    region.idx <- which(headers == "Region")
-    ## some rows of table only have a single column referencing
-    ## another row of the table, need to filter these out
-    ## country.names <- xml_text(xml_find_all(iso.code.table,
-    ##                          ".//tr[count(td)>2]/td[1]/a[1]"))
-    lga.name <- xml_text(xml_find_all(lga.table,
-                                      ".//tr/td[1]/a[1]"))
-    region.name <- xml_text(xml_find_all(lga.table,
-                                         paste0(".//tr/td[", region.idx, "]/a[1]")))
-    if (state == "Western_Australia")
-    {   # extra table for perth
-        lga.table <- xml_find_first(pl, "//table[@class='wikitable sortable']")
-        perth.lga <- xml_text(xml_find_all(lga.table,
-                                           ".//tr/td[1]/a[1]"))
-        region.name <- c(rep("Perth", length(perth.lga)), region.name)
-        lga.name <- c(perth.lga, lga.name)
-    }
+    lga.tables <- xml_find_all(pl, "//table[@class='wikitable sortable']")
+    lga.name <- trimws(xml_text(xml_find_all(lga.tables,
+                                      ".//tr/td[1]")))
+    region.name <- unlist(lapply(lga.tables, findRegions))
     ## replace unicode dash with standard hyphen
     lga.name <- gsub("\\p{Pd}", "-", lga.name, perl = TRUE)
     region.name <- gsub("\\p{Pd}", "-", region.name, perl = TRUE)
-    lgas <- rbind(data.frame(state = gsub("_", " ", state), region = region.name,
-                             lga = lga.name), lgas)
+    lgas <- rbind(lgas, data.frame(state = states[i+1],  # +1 since No ACT
+                                   region = region.name, lga = lga.name))
 }
 
-save(australia.zip.codes, file = "data/australia.zip.codes.rda", compress = TRUE)
+## Use geocoding to look up approximate lat/long for each region/LGA
+patt <- paste0(",? ?", "(Regional Council|City Council|Communities Authority|",
+               "Town Council|Community Council|District Council|Community Government Council|",
+               "(?:Rural )?City|(?:Aboriginal )?Shire(?: Council)?|Region(?:al)?|Town",
+               "|Community|Council|Corporation|Municipality)",
+               "( of)? ?")
+lga.lookup <- sub(patt, "", lgas[["lga"]], perl = TRUE)
+## lga.lookup <- sub("^([A-z -]*), ([A-z ]* of)$", "\\2 \\1", lgas[["lga"]])
+address <- paste(lga.lookup, lgas[["state"]], "Australia", sep = ", ")
+## Fix a couple addresses geocoder doesn't like
+address <- sub("^Hunter's Hill", "Hunters Hill", address)
+address <- sub("^Mid-Western, New", "Mudgee, New", address)
+address <- sub("^Unincorporated Far West, New", "Tibooburra, New", address)
+lga.lat.long <- tidygeocoder::geo(address)
+
+## For each place find nearest LGA, exclude islands (LGAs with no region) and
+## two unicorporated areas of Darwin, NT
+missing.coords <- is.na(lga.lat.long[["lat"]]) | lgas[["region"]] == ""
+lat.long.no.islands <- lga.lat.long[!missing.coords, ]
+nearest.coords <- FNN::get.knnx(data = lat.long.no.islands[, c("lat", "long")],
+                             query = australia.post.codes[, c("latitude", "longitude")],
+                             k = 1)
+nearest.idx <- drop(nearest.coords$nn.index)
+nearest.dist <- nearest.coords$nn.dist
+
+################################################################################
+## Geocoding doesn't do quite accurate enough job, so get LGAs by reverse geocoding
+## LGAs are available in addresses returned by default API used by tidygeocoder
+##
+## For region info, still use Wikipedia tables
+
+## Get LGA for every postcode
+## WARNING: the following takes hours as it's pulling adddresses for 3313 post
+## codes/2,871 (lat,long) pairs
+first.post.code.idx <- !duplicated(australia.post.codes[["post.code"]])
+pc.addresses <- reverse_geocode(australia.post.codes[first.post.code.idx, ],
+                                lat = "latitude", long = "longitude")$address
+## Assumes all addresses end with format '[lga], [state], [country]'
+## or '[lga], [state], [postcode], [country]'
+pc.lgas <- sub("^(?:.*, )?([A-z() '-]*), [A-z() -]*, ((?:NT )?[0-9]*, )?Australia$",
+               "\\1", pc.addresses, perl = TRUE)
+## Fix two edge cases, one from ACT another for an unicorporated island of Victoria
+pc.lgas[pc.lgas %in% "Tidbinbilla Road"] <- "Australian Capital Territory"
+unincorporated <- c("French Island", "Unincorporated Area Torrens Island",
+                    "Pastoral Unincorporated Area")
+pc.lgas[pc.lgas %in% unincorporated] <- "Unicorporated Area"
+unique.lgas <- unique(pc.lgas)
+post.codes <- australia.post.codes[["post.code"]]
+unique.post.codes <- post.codes[first.post.code.idx]
+names(pc.lgas) <- unique.post.codes
+## dplyr::recode(post.codes, !!!pc.lgas)
+lga.vec <- character(length(post.codes))
+for (i in unique.post.codes) {
+    lga.vec[post.codes %in% i] <- pc.lgas[as.character(i)]
+}
+australia.post.codes[["LGA"]] <- lga.vec
+
+
+## (very) roughly, which places have been missclassified?
+## worst10.idx <- order(nearest.dist, decreasing = TRUE)[1:10]
+## cbind(lgas[["region"]][nearest.idx[worst10.idx]],
+##       australia.post.codes[worst10.idx, "place"])
+## head(sort(nearest.coords$nn.dist, decreasing = TRUE))
+## DIST.CUTOFF <- 1
+## potential.miss <- which(nearest.dist > DIST.CUTOFF)
+## best.bad <- which.min(nearest.dist[potential.miss])
+## ## verify Tablederry is correctly classified into Barcaldine Region LGA
+## c(lgas[nearest.idx[potential.miss[best.bad]], ], australia.post.codes[bad.idx[potential.miss], ])
+## ## Use reverse geocoding to get LGA for places with dist above cut-off
+## ## Reverse geocoding every place in the data.frame would be prohibitively slow
+## addresses <- reverse_geocode(australia.post.codes[potential.miss,],
+##                              lat = "latitude", long = "longitude")$address
+## ## missing.lgas <- vapply(strsplit(addresses, ", "), `[`, "", 2)
+## ## All addresses end with format '[lga], [state], [country]' or '[lga], [state], [postcode], [country]'
+## missing.lgas <- sub("^.*, ([A-z() -]*), [A-z() -]*, ([0-9]*, )?Australia$",
+##                     "\\1", addresses)
+## missing.lgas <- sub("Regional$", "Region", missing.lgas)
+## missing.lgas <- sub("Council$", "", missing.lgas)
+## ## LGAs from wikipedia have format 'Yalgoo, Shire of', 'Vincent, City of', etc.
+## missing.lgas <- sub("([A-z]*) [Oo]f (.*)$", "\\2, \\1 of", missing.lgas)
+## ## cbind(australia.post.codes[potential.miss, "place"], addresses)
+## australia.post.codes$lga <- lgas[["lga"]][nearest.idx]
+## act.idx <- australia.post.codes[["state"]] %in% "Australian Capital Territory"
+## australia.post.codes$lga[act.idx] <- NA  # No LGA for ACT
+## australia.post.codes[["lga"]][potential.miss] <- missing.lgas
+
+## For region info, still use Wikipedia tables
+australia.post.codes$region <- lgas[["region"]][nearest.idx]
+missing.idx <- !nzchar(australia.post.codes[["region"]])
+australia.post.codes[["region"]][missing.idx] <- NA
+act.idx <- australia.post.codes[["state"]] %in% "Australian Capital Territory"
+australia.post.codes[["region"]][act.idx] <- "Australian Capital Territory"
+australia.post.codes[["region"]] <- as.factor(australia.post.codes[["region"]])
+
+save(australia.post.codes, file = "data/australia.post.codes.rda", compress = TRUE)

88971b177c162f44636536de9d7923219cffac7a
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-11-30 13:20:49 +1100

    DS-3517: Add tests for levenshtein dist. matching

--- tests/testthat/test-recodegeography.R
+++ tests/testthat/test-recodegeography.R
@@ -29,3 +29,32 @@ test_that("Postcode to place conversion",
     out.expect <- us.zip.codes[c(idx1, idx2), "place"]
     expect_equal(out, out.expect)
 })
+
+test_that("Approx. matching with levenshtein dist.",
+{
+    data(us.zip.codes, package = "flipGeoData")
+    txt <- us.zip.codes[["place"]]
+    idx <- c(4L, 5L, 8L, 10L)
+    txt <- txt[idx]
+    txt[1] <- "King Kove"
+    txt[3] <- "Saint Georg Island"
+    txt[4] <- "St. Paul Island"
+    out <- RecodeGeography(txt, region = "USA", input.type = "Place",
+                           output.type = "Zip Code",
+                           max.levenshtein.dist = 0)
+    expect.out <- rep(NA, length(txt))
+    expect.out[2] <- us.zip.codes[idx[2], "zip.code"]
+    expect_equal(out, expect.out)
+
+    out <- RecodeGeography(txt, region = "USA", input.type = "Place",
+                           output.type = "Zip Code",
+                           max.levenshtein.dist = 1)
+    expect.out[1:3] <- us.zip.codes[idx[1:3], "zip.code"]
+    expect_equal(out, expect.out)
+
+    out <- RecodeGeography(txt, region = "USA", input.type = "Place",
+                           output.type = "Zip Code",
+                           max.levenshtein.dist = 4)
+    expect.out[4] <- us.zip.codes[idx[4], "zip.code"]
+    expect_equal(out, expect.out)
+})

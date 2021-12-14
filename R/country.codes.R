0e49570f69bf279b869411baec13b51ea1a50ba4
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-12-14 16:47:33 +1100

    DS-3540: Remove reference to synonyms data set

--- R/country.codes.R
+++ R/country.codes.R
@@ -9,7 +9,7 @@
 #' \item country.code - two-character country code for the country in ISO-3166 format.
 #' \item in.europe - logical indicating whether the country is located in Europe.
 #' \item post.codes.available - logical indicating whether post code information is available for the country in one of the post code data files in the package.
-#' \item synonyms.available - logical indicating whether synonym place names are available in \code{\link{synonyms}}
+#' \item synonyms.available - logical indicating whether synonym place names are available from \url{https://download.geonames.org/export/zip/}.
 #' }
 #' @name country.codes
 #' @format A data.frame with 249 rows and 4 columns.
